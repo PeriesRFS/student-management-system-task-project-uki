d81dd4424c9d797eb73dd2080aa7d27791dfb324
PeriesRFS <144052384+PeriesRFS@users.noreply.github.com>
2026-02-11 11:47:57 +0530

    Add JavaScript Logic to- Fetching, Adding and Deleting Students
    Implement frontend logic to
    1.Load and display students
    2.Add new students using form
    3.Delete students by ID
    and Basic error handling

--- frontend/script.js
+++ frontend/script.js
@@ -1,13 +1,67 @@
-
 const API_URL = "";
 const form = document.getElementById("studentForm");
 const tableBody = document.getElementById("studentTable");
 
-//Load and Display student-list 
+//Load and Display student-list - when the page loads
+//Load Students
+window.onboard = fetchStudents;
 
+//Fetch and Display students
+function fetchStudents() {
+  fetch(API_URL)
+    .then((res) => res.json())
+    .then((data) => {
+      tableBody.innerHTML = "";
+      data.forEach((student) => {
+        const row = `
+                <tr>
+                    <td>${student.id}</td>
+                    <td>${student.name} </td>
+                    <td>${student.email}</td>
+                    <td>${student.course}</td>
+                    <td>
+                        <button onclick="deleteStudent(${student.id})">Delete</button>
+                    </td>
+                </tr>
+            `;
+        tableBody.innerHTML += row;
+      });
+    });
+}
 
 //Add Student
+form.addEventListener("submit", function (e) {
+  e.preventDefault();
 
+  //create a student object
+  const student = {
+    name: document.getElementById("name").value,
+    email: document.getElementById("email").value,
+    course: document.getElementById("course").value,
+  };
 
-//Delete Students
+  //Send a POST request to the API to add a new student
+  fetch(API_URL, {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify(student),
+  })
+    
+    //checking if the request sent was sucessful
+    .then((res) => {
+      if (!res.ok) throw new Error("Failled to add student");
+      return res.json();
+    });
+});
 
+//Delete Students using their id
+function deletStudents(id) {
+
+  //send the delete request to api with student id
+  fetch(API_URL + id, { method: "DELETE" })
+  
+    //check if the delete was sucessful
+    .then((res) => {
+      if (!res.ok) throw new Error("Delete failed");
+    });
+}
