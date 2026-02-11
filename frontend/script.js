3a66bc0fbc75b150dcdcdf5144d595eb80a0bc15
PeriesRFS <144052384+PeriesRFS@users.noreply.github.com>
2026-02-11 09:53:46 +0530

    Initiate Frontend JavaScript file Structure
    Initiating the frontend JavaScript file by Adding
    1.API URL linking reference
    2.DOM element  references
    
    and Link the JavaScript file with the respective HTML file

---
+++ frontend/script.js
@@ -0,0 +1,13 @@
+
+const API_URL = "";
+const form = document.getElementById("studentForm");
+const tableBody = document.getElementById("studentTable");
+
+//Load and Display student-list 
+
+
+//Add Student
+
+
+//Delete Students
+
