7ffa95eb25bf7bb3968e5165ff1fce4d0a9b5588
PeriesRFS <144052384+PeriesRFS@users.noreply.github.com>
2026-02-11 13:02:35 +0530

    Add Models for Student data
    Define Models to
    1.Creating student data (StudentCreate) including input validation and examples.
    2.Returning student data  output (StudentOut)

--- backend/main.py
+++ backend/main.py
@@ -1,8 +1,22 @@
 #Import Libraries
 from fastapi import FastAPI, HTTPException,status
+from pydantic import BaseModel, EmailStr, Field
 
 #initialize FASTAPI app
 app = FastAPI(title = "Student Management API", version ="1.0.0")
+
+#MODELS
+
+#Model for Create A New Student
+class StudentCreate(BaseModel):
+    name: str = Field(..., min_length = 1, example = "Joyson Peries")
+    email: EmailStr = Field(..., example = "joys@gmail.com")
+    course: str = Field(..., min_length = 1, example = "Machine Learning")
+
+#Model for return student Data with Id
+
+class StudentOut(StudentCreate):
+    id: int
 
 #Routes
 
@@ -10,3 +24,5 @@ app = FastAPI(title = "Student Management API", version ="1.0.0")
 @app.get("/")
 def root():
     return {"message":"API is Running"}
+
+#Create A New Student 
